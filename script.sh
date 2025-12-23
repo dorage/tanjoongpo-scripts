e704c326f784ffb5db03cf3fa41b3005d410b0d4
kanghyunlee <kanghyunlee@kanghyunleeui-MacBookAir.local>
2025-12-23 18:56:18 +0900

    feat: mysql을 한 세션에서 열어야함

--- script.sh
+++ script.sh
@@ -2,52 +2,49 @@
 
 set -eo pipefail
 
-touch $log_filename
-log_filename="$today.log"
+yesterday="$( date +%Y )$( date +%m )$(($(date +%d) - 1))"
+today="$( date +%Y )$( date +%m )$( date +%d )"
 
-function echo() {
+log_filename="$today.log"
+touch "$log_filename"
+
+function log() {
 	msg=$1
 	timestamp=$(date '+%Y-%m-%d %H:%M:%S')
 
 	echo "[$timestamp] $msg" >> $log_filename
 }
 
-yesterday="$( date +%Y )$( date +%m )$(($(date +%d) - 1))"
-today="$( date +%Y )$( date +%m )$( date +%d )"
-
 mbr_filename="${today}_001_mobile_mtchg.json"
-filename="mbr_$today.json"
+mbr_only_filename="mbr_$today.json"
 
-echo "탄중포 오늘의 작업 시작"
+log "탄중포 오늘의 작업 시작"
 
 # mbr 데이터만 추출
-cat $file | jq '.result | .mbr' > "$filename"
+cat $mbr_filename | jq '.result | .mbr' > "$mbr_only_filename"
 
-echo "DUCKDB: MBR 테이블 생성 시작"
+log "DUCKDB: MBR 테이블 생성 시작"
 
 # mbrs 데이터를 db로 생성
-duckdb $today.duckdb "CREATE TABLE mbrs AS SELECT * FROM read_json('$filename')"
+duckdb $today.duckdb "CREATE TABLE mbrs AS SELECT * FROM read_json('$mbr_only_filename')"
 
-echo "DUCKDB: MBR 테이블 생성 완료"
-echo "DUCKDB: ACR 테이블 생성 시작"
+log "DUCKDB: MBR 테이블 생성 완료"
+log "DUCKDB: ACR 테이블 생성 시작"
 
 # acrs 테이블 생성
 duckdb $today.duckdb "CREATE TABLE acrs (mtchgId varchar(255), incntvCd varchar(255), acrsCo int, dtlAcrs int)"
 
-echo "DUCKDB: ACR 테이블 생성 완료"
-echo "DUCKDB: 프로덕션 MYSQL 연결 시작"
+log "DUCKDB: ACR 테이블 생성 완료"
+log "DUCKDB: 데이터 매칭 시작"
 
 # mysql 데이터 끌어오기 위해 연결
 duckdb $today.duckdb "INSTALL mysql"
 duckdb $today.duckdb "LOAD mysql"
-duckdb $today.duckdb "ATTACH 'host=175.126.82.217 port=3306 user=readonly password=Kj7#mN9@pQ database=circularlabs' AS mysqldb (TYPE mysql)"
-duckdb $today.duckdb "USE mysqldb"
 
-echo "DUCKDB: 프로덕션 MYSQL 연결 완료"
-echo "DUCKDB: ACR 테이블에 데이터 생성 시작"
-
-# acrs 데이터 쌓기
 duckdb $today.duckdb "
+ATTACH 'host=175.126.82.217 port=3306 user=readonly password=Kj7#mN9@pQ database=circularlabs' AS mysqldb (TYPE mysql);
+USE mysqldb;
+
 INSERT INTO db.acrs (mtchgId, incntvCd, acrsCo, dtlAcrs)
 SELECT m.mtchgId as mtchgId, 'I0003' as incntvCd, r.count as acrsCo, 0 as dtlAcrs FROM mysql_query(
 	'mysqldb', 
@@ -56,13 +53,14 @@ SELECT m.mtchgId as mtchgId, 'I0003' as incntvCd, r.count as acrsCo, 0 as dtlAcr
 	WHERE DATE(r.created_at)='$today'
 	GROUP BY r.user_name, r.user_phone'
 ) r
-LEFT JOIN db.mbrs m ON m.usernm=r.name and m.moblphon=r.phone"
+LEFT JOIN db.mbrs m ON m.usernm=r.name and m.moblphon=r.phone;
+"
 
-echo "DUCKDB: ACR 테이블에 데이터 생성 완료"
-echo "DUCKDB: ACR 테이블 JSON으로 내보내기 시작"
+log "DUCKDB: 데이터 매칭 완료"
+log "DUCKDB: ACR 테이블 JSON으로 내보내기 시작"
 
 # acrs json으로 생성
 duckdb $today.duckdb "COPY db.acrs TO '$today-001-E0960.json'"
 
-echo "DUCKDB: ACR 테이블 JSON으로 내보내기 완료"
-echo "오늘의 탄중포 작업 완료"
+log "DUCKDB: ACR 테이블 JSON으로 내보내기 완료"
+log "오늘의 탄중포 작업 완료"
